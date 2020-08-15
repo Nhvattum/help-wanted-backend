b41b5db762eccbad92f3a8a3728b67b3bf5956e3
Nick Hvattum <nhvattum@gmail.com>
2020-08-15 09:17:39 -0700

    about to pull from upstream

--- server.js
+++ server.js
@@ -1,64 +1,49 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const passport = require("passport")
-const bodyParser = require("body-parser") 
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParser = require('body-parser')
 
-// initilize Express server
+// initialize Express server
 const app = express()
 
-//  require router
- const users = require('./routes/users')
- const events = require('./routes/events')
- const comments = require('./routes/comments')
- const tracks = require('./routes/tracks')
+// require router
+const users = require('./routes/users')
 
-//  middleware to CORS requests
+// middleware to allow to CORS requests
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
     next()
 })
 
-// bodyParser middleware 
+// bodyParser middleware
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-//  config our DB
-const uri = process.env.MONGOD_URI
+// config our db
+const db = process.env.MONGOD_URI
 
-//console.log(uri)
-
-const MongoClient = require('mongodb').MongoClient;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-// connect to Mongodb( using Atlas)
-mongoose.connect(uri)
-    .then((() => console.log('MongoDB conneted...🏈')))
+// connect to mongodb (using Atlas)
+mongoose.connect(db)
+    .then((() => console.log('MongoDB connected... 🥭')))
     .catch(err => console.log(err))
 
-//  test routing
-app.get('/', function(req, res) { 
-    res.send("Hello, World! Server is up and running 🥎 ")
+// test routing
+app.get('/', (req, res) => {
+    res.send("Hello world!\n Server is up and running 👍")
 })
 
-//  passport middleware
+// passport middleware
 app.use(passport.initialize())
 
-//  passport JWT token set/config
+// passport JWT token set/config
 require('./config/passport')(passport)
 
-//  setup out routes
+// setup our routes
 app.use('/users', users)
-app.use('/events', events)
-app.use('/comments', comments)
-app.use('/tracks', tracks)
+
 
 
 // start our sever
